02ac548ab3778bf4b51c3eac9c081d2381671b2d
tvchung <hna.tvchung@gmail.com>
2025-09-03 11:29:04 +0700

    Day01

---
+++ Day01/tvc-lesson01/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1> Thông tin sinh viên</h1>
+        <hr/>
+        <div className='alert alert-danger'>
+          <p>Mã sinh viên: 22001235</p>
+          <p>Họ và tên: Chung Trịnh</p>
+          <p>Email: chungtrinhj@gmail.com</p>
+          <p>Phone: 0978611889</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
